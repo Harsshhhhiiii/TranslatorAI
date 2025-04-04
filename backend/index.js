e5c9dd72066062e2ed1e2434f65b56bd6c25a789
Harsshhhhiiii <harshit.student.min21@itbhu.ac.in>
2025-04-04 21:58:34 +0530

    iidk

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import imageRoutes from "./routes/imageRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors({
