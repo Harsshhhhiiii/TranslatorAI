724ed7a8991d28671c689d9e3bb09f4412c44743
Harsshhhhiiii <harshit.student.min21@itbhu.ac.in>
2025-04-05 12:57:38 +0530

    Final

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',"https://translator-ai-nine.vercel.app","https://translator-ai-harshits-projects-99ccb490.vercel.app"],
     credentials: true
 }));
 app.use(express.json());
